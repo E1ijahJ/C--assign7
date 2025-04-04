f1aa35ac7d617b60755fdfeb7d69eff646179f08
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-03 22:42:10 -0400

    Implemented test for my floattoint32

--- tests/TypeInterpret_tests.cpp
+++ tests/TypeInterpret_tests.cpp
@@ -9,3 +9,12 @@ TEST(TypeInterpretTest, Int32ToFloatCorrectness) {
 
     EXPECT_FLOAT_EQ(result, 1.23f);
 }
+
+
+TEST(TypeInterpretTest, FloatToInt32Correctness) {
+    float input = 1.23f;
+    int32_t result = floatToInt32(input);
+
+    
+    EXPECT_EQ(result, 0x3F9D70A4);
+}
