e331091484b1ae216129aec6869747e767e72207
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-03 23:10:54 -0400

    Declared said function for double to int64

--- include/TypeInterpret.h
+++ include/TypeInterpret.h
@@ -6,3 +6,4 @@ int32_t floatToInt32(float input);
 float uint32ToFloat(uint32_t input);
 uint32_t floatToUint32(float input);
 double int64ToDouble(int64_t input);
+int64_t doubleToInt64(double input);
